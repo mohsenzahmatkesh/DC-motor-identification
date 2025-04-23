531de004b53fe4cf9e213b530699ddf08da5916f
mohsenzahmatkesh <mhn.zahmat@gmail.com>
2025-04-23 08:10:59 +0100

    Update SMC

--- matlab/motor_control_SMC.m
+++ matlab/motor_control_SMC.m
@@ -1,27 +1,5 @@
-clc; clear; close all
-
-%% === Your 2-State System ===
-% A = [-44.58, -31.47;
-%       16,     0];
-% 
-% B = [8;
-%      0];
-% 
-% C = [3.958, 3.936];
-% D = 0;
-
-% A = [-323.7,  -109;
-%        64,      0];
-% 
-% B = [4;
-%      0];
-% 
-% C = [4.291, 1.357];
-% 
-% D = 0.9575;
-% 
-% sys_ss = ss(A, B, C, D);
-
+clear
+close all
 A = [-135.2,  -44.69;
         32,      0];
 
@@ -38,9 +16,9 @@ sys_ss = ss(A, B, C, D);
 % lambda = 250;
 % eta = 34;
 % epsilon = 0.7;
-lambda = 200;
+lambda = 250;
 eta = 55;
-epsilon = 1.5;
+epsilon = 3;
 
 %% === Simulation Settings ===
 % dt = 0.001;
@@ -53,7 +31,22 @@ x = [0; 0];  % Initial 2-state system
 e_int = 0;
 % x2_ref = 1 * ones(size(t));  % Step reference for velocity
 f = 1;  % 1 Hz sinusoidal reference
-x2_ref = sin(2 * pi * f * t); 
+% x2_ref = sin(2 * pi * f * t); 
+% x2_ref = 0.5 * (t > 2) + 0.3 * sin(4 * pi * t);  % Step at t=2 + oscillation
+% x2_ref = sawtooth(2 * pi * f * t, 0.5);  % Triangle wave with period 1s
+% x2_ref = zeros(size(t));
+% x2_ref(t < 3) = 5 * sin(2 * pi * 1 * t(t < 3));         % [-5, +5] low-freq sine
+% x2_ref(t >= 3 & t < 6) = 10;                           % step to +10
+% x2_ref(t >= 6) = 3 * sin(2 * pi * 3 * t(t >= 6));      % [-3, +3] fast sine
+x2_ref = zeros(size(t));
+x2_ref(t < 3) = 0.5 * sin(2 * pi * 1 * t(t < 3));
+x2_ref(t >= 3 & t < 6) = 1;
+x2_ref(t >= 6) = 0.3 * sin(2 * pi * 3 * t(t >= 6));
+
+
+
+
+
 
 x_hist = zeros(n, 2);
 u_hist = zeros(n, 1);
@@ -86,9 +79,15 @@ for k = 1:n
     u_hist(k) = u;
     s_hist(k) = s;
 end
+x_hist_smc = x_hist;
+u_hist_smc = u_hist;
+x2_ref_smc = x2_ref;
 
+function y = sat(x)
+    y = tanh(x) .* min(abs(x), 1);
+end
 %% === Plot Results ===
-figure('Color','w', 'Position', [100 100 1200 800]);
+figure('Color','w', 'Position', [100 100 1200 1200]);
 
 tiledlayout(3, 1, 'TileSpacing', 'tight', 'Padding', 'tight');  % 3 rows, 1 column
 
@@ -111,6 +110,7 @@ title('Control Effort', 'FontSize', 20);
 grid on
 ax = gca;
 ax.FontSize = 24;
+hold on
 
 % --- Bottom Plot: Sliding Surface ---
 nexttile
@@ -121,17 +121,6 @@ title('Sliding Surface Evolution', 'FontSize', 20);
 grid on
 ax = gca;
 ax.FontSize = 24;
-
-
-%% === Saturation Function ===
-% function y = sat(x)
-%     y = tanh(x) .* min(abs(x), 1);
-% end
-
-function y = sat(x)
-    y = x / (abs(x) + 1);  % smooth SMC formulation
-end
-
 
 
 
